2059ff87ba9cf59e999d5f7e817808b7f005cee5
Antonio Marcos <marcosgeo@yahoo.com>
2023-06-08 09:32:51 -0300

    1 - basic angular component with the map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,30 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+})
+export class MapComponent implements AfterViewInit {
+  map?: Map;
+
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([-46.5, -23.5]),
+        zoom: 4,
+      }),
+    });
+  }
+}
